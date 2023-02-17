0dc2d6f8769b987a0dea5fefc71fb6629a1bd8a6
Jean-François Lemieux <58086398+jflem47@users.noreply.github.com>
2023-02-16 21:01:12 -0500

    Update ft_strjoin.c

--- libft/ft_strjoin.c
+++ libft/ft_strjoin.c
@@ -19,7 +19,9 @@ char	*ft_strjoin(char const *s1, char const *s2)
 	char	*res;
 	int		i;
 	int		j;
-
+	
+	if (!s1 || !s2)
+		return (NULL);
 	s1_len = ft_strlen(s1);
 	s2_len = ft_strlen(s2);
 	res = malloc(s1_len + s2_len + 1);
